e3027c3f1b55153eb5eeccb3859e9691d89a7db9
Sumit <veerkhanduri@gmail.com>
2023-09-21 16:00:15 -0700

    added a background and a start button the gui

--- Project/Form1.Designer.cs
+++ Project/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms.DataVisualization.Charting;
+﻿using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Project
 {
@@ -37,12 +38,19 @@ namespace Project
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
@@ -51,29 +59,17 @@ namespace Project
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(0, 2);
+            this.chart1.Location = new System.Drawing.Point(132, 0);
+            this.chart1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(1580, 332);
+            this.chart1.Size = new System.Drawing.Size(1860, 380);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
-            this.chart1.Click += new System.EventHandler(this.chart1_Click);
-
-            chart1.ChartAreas["ChartArea1"].AxisX.ScaleView.Zoomable = true;
-            chart1.ChartAreas["ChartArea1"].AxisY.ScaleView.Zoomable = true;
-            chart1.ChartAreas["ChartArea1"].CursorX.AutoScroll = true;
-            chart1.ChartAreas["ChartArea1"].CursorY.AutoScroll = true;
-            chart1.ChartAreas["ChartArea1"].AxisX.ScrollBar.IsPositionedInside = false;
-            chart1.ChartAreas["ChartArea1"].AxisY.ScrollBar.IsPositionedInside = false;
-            chart1.ChartAreas["ChartArea1"].AxisX.ScaleView.SmallScrollMinSizeType = DateTimeIntervalType.Number;
-            chart1.ChartAreas["ChartArea1"].AxisX.ScaleView.SmallScrollMinSize = 1;
-            chart1.ChartAreas["ChartArea1"].AxisY.ScaleView.SmallScrollMinSizeType = DateTimeIntervalType.Number;
-            chart1.ChartAreas["ChartArea1"].AxisY.ScaleView.SmallScrollMinSize = 1;
-            
             
             // 
             // timer1
@@ -86,46 +82,155 @@ namespace Project
             this.chart2.ChartAreas.Add(chartArea2);
             legend2.Name = "Legend1";
             this.chart2.Legends.Add(legend2);
-            this.chart2.Location = new System.Drawing.Point(0, 340);
+            this.chart2.Location = new System.Drawing.Point(132, 778);
+            this.chart2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
             series2.ChartArea = "ChartArea1";
             series2.Legend = "Legend1";
             series2.Name = "Series1";
             this.chart2.Series.Add(series2);
-            this.chart2.Size = new System.Drawing.Size(893, 403);
+            this.chart2.Size = new System.Drawing.Size(1866, 334);
             this.chart2.TabIndex = 1;
             this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
             // 
-            // hScrollBar1
+            // chart3
             // 
-            /*this.hScrollBar1.Location = new System.Drawing.Point(0, 0);
-            this.hScrollBar1.Name = "hScrollBar1";
-            this.hScrollBar1.Size = new System.Drawing.Size(80, 21);
-            this.hScrollBar1.TabIndex = 0;*/
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(132, 403);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(1866, 344);
+            this.chart3.TabIndex = 3;
+            this.chart3.Text = "chart3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2015, 745);
-            this.Controls.Add(this.hScrollBar1);
+            this.ClientSize = new System.Drawing.Size(2244, 1160);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart3);
             this.Controls.Add(this.chart2);
             this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             this.ResumeLayout(false);
 
         }
 
+
+        private void generateInfo()
+        {
+
+            
+            //time-amplitude graph 1
+            this.chart1.ChartAreas[0].AxisX.Title = "time";
+            this.chart1.ChartAreas[0].AxisY.Title = "Amplitude";
+            var chartArea1 = this.chart1.ChartAreas[0];
+            chartArea1.AxisX.MinorGrid.Enabled = true;
+            this.chart1.Series[0].BorderWidth = 3;
+            
+            this.chart1.Series[0].Color = Color.Yellow;
+            
+
+            //add color
+            this.chart1.ChartAreas[0].BackColor = Color.Black;
+            this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LawnGreen;
+            this.chart1.ChartAreas[0].AxisX2.MajorGrid.LineColor = Color.LawnGreen;
+            chartArea1.AxisX.LineWidth = 2;
+            this.chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LawnGreen;
+            
+
+            // set view range to [0,max]
+            chartArea1.AxisX.Minimum = 0;
+            
+
+            // enable autoscroll
+            chartArea1.CursorX.AutoScroll = true;
+
+            // let's zoom to [0,blockSize] (e.g. [0,100])
+            chartArea1.AxisX.ScaleView.Zoomable = true;
+            chartArea1.AxisX.ScaleView.SizeType = DateTimeIntervalType.Number;
+            int position = 0;
+            int size = 10;
+            chartArea1.AxisX.ScaleView.Zoom(position, size);
+
+            // disable zoom-reset button (only scrollbar's arrows are available)
+            chartArea1.AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.SmallScroll;
+
+            // set scrollbar small change to blockSize (e.g. 100)
+            chartArea1.AxisX.ScaleView.SmallScrollSize = 10;
+            chartArea1.AxisX.ScrollBar.ButtonColor = Color.WhiteSmoke;
+            chartArea1.AxisX.ScrollBar.LineColor = Color.White;
+            chartArea1.AxisX.ScrollBar.IsPositionedInside = false;
+
+
+            //time-amplitude graph 2
+            this.chart3.ChartAreas[0].AxisX.Title = "time";
+            this.chart3.ChartAreas[0].AxisY.Title = "Amplitude";
+            var chartArea3 = this.chart3.ChartAreas[0];
+
+            // set view range to [0,max]
+            chartArea3.AxisX.Minimum = 0;
+            
+
+            // enable autoscroll
+            chartArea3.CursorX.AutoScroll = true;
+
+            // let's zoom to [0,blockSize] (e.g. [0,100])
+            chartArea3.AxisX.ScaleView.Zoomable = true;
+            chartArea3.AxisX.ScaleView.SizeType = DateTimeIntervalType.Number;
+            
+            chartArea3.AxisX.ScaleView.Zoom(position, size);
+
+            // disable zoom-reset button (only scrollbar's arrows are available)
+            chartArea3.AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.SmallScroll;
+
+            // set scrollbar small change to blockSize (e.g. 100)
+            chartArea3.AxisX.ScaleView.SmallScrollSize = 10;
+
+
+
+            //freq-amplitude graph
+            this.chart2.ChartAreas[0].AxisX.Title = "frequency";
+            this.chart2.ChartAreas[0].AxisY.Title = "Amplitude";
+            this.chart2.ChartAreas[0].AxisX.Minimum = 0;
+        }
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
-        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private Chart chart3;
+        private System.Windows.Forms.Button button1;
     }
+
+    
 }
 
--- Project/Form1.cs
+++ Project/Form1.cs
@@ -14,55 +14,77 @@ namespace Project
 {
     public partial class Form1 : Form
     {
-        
+        bool flag = false;
         int count = 0;
         int N = 10;
         int f = 3;
+        int numberOfSamples = 100;
 
         public Form1()
         {
+            
             InitializeComponent();
+        
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-           timer1.Interval = 1;
+            generateInfo();
+            timer1.Interval = 1;
             timer1.Start();
-            
+
         }
 
 
         private void CreateAmplitudeChart(double[] s)
         {
-
+            
             for (int t = 0; t <s.Length; t++)
             {
                 chart1.Series[0].Points.AddXY(t+count, s[t]);
+                chart3.Series[0].Points.AddXY(t + count, s[t]);
             }
             count += s.Length;
     }
 
-        private void chart1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            /*if (count > 10)
+            if (count > numberOfSamples)
             {
                 timer1.Stop();
-            }*/
+            }
             UpdateChart();
         }
 
         private void UpdateChart()
         {
-            double[] s = calculateSamples(f, N);
-            CreateAmplitudeChart(s);
-            CreateFreqChart(s, N);
+            if (flag)
+            {
+                double[] s = calculateSamples(f, N);
+                CreateAmplitudeChart(s);
+                CreateFreqChart(s, N);
+                int width = chart1.Width;
+                int height = chart1.Height;
+                chart1.Size = new Size(width++, height++);
+            } else
+            {
+                displayDefault();
+            }
             
+
+
+
+        }
+
+        private void displayDefault()
+        {
+            timer1.Stop();
+                chart1.Series[0].Points.AddXY(0, 0);
+                chart3.Series[0].Points.AddXY(0 , 0);
+                chart2.Series[0].Points.AddXY (0 , 0);
+            
+
         }
 
         private double[] calculateSamples(int f, int N)
@@ -100,13 +122,7 @@ namespace Project
             return A;
         }
 
-        /*private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
-        {
-            int newPosition = e.NewValue;
-            chart1.ChartAreas[0].AxisX.Minimum = newPosition;
-            chart1.ChartAreas[0].AxisX.Maximum = newPosition + 1000;
-            
-        }*/
+    
 
         private void ChartArea1_AxisViewChanged(object sender, ViewEventArgs e)
         {
@@ -114,5 +130,23 @@ namespace Project
             // You can update your chart's data based on the new view range
         }
 
+        private void chart2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            
+            timer1.Start();
+            
+            
+        }
     }
 }
