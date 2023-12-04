76c3da255db7ec6c56cbecfb51cd23e180f6fbff
Sumit <veerkhanduri@gmail.com>
2023-12-04 10:04:45 -0800

    removed Naudio

--- Project/Form1.cs
+++ Project/Form1.cs
@@ -17,7 +17,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
-using NAudio.Wave;
+
 
 namespace Project
 {
